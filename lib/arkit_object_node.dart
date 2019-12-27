e4fde4cbe82082ecc1b739a0c06fe4faaefbe37c
小野達磨 <>
2019-12-27 11:11:35 +0900

    3Dオブジェクトを作成するarkit_object_node.dartファイルを追加

---
+++ lib/arkit_object_node.dart
@@ -0,0 +1,35 @@
+import 'package:arkit_plugin/arkit_node.dart';
+import 'package:arkit_plugin/light/arkit_light.dart';
+import 'package:arkit_plugin/physics/arkit_physics_body.dart';
+import 'package:flutter/widgets.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+///  Node that references an external serialized node graph.
+class ARKitObjectNode extends ARKitNode {
+  ARKitObjectNode({
+    @required this.localPath,
+    ARKitPhysicsBody physicsBody,
+    ARKitLight light,
+    Vector3 position,
+    Vector3 scale,
+    Vector3 eulerAngles,
+    String name,
+    int renderingOrder,
+  }) : super(
+          physicsBody: physicsBody,
+          light: light,
+          position: position,
+          scale: scale,
+          eulerAngles: eulerAngles,
+          name: name,
+          renderingOrder: renderingOrder,
+        );
+
+  /// Node localPath at bundle.
+  final String localPath;
+
+  @override
+  Map<String, dynamic> toMap() => <String, dynamic>{
+        'localPath': localPath,
+      }..addAll(super.toMap());
+}
